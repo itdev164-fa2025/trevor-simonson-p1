27218b659ea12ea2e43e910e62d859dde6c9d236
Trevor Simonson <simonsot@gmatc.matc.edu>
2024-09-30 13:21:32 -0500

    Unit gallery displaying artwork

--- gallery/src/templates/unit-gallery.js
+++ gallery/src/templates/unit-gallery.js
@@ -2,14 +2,28 @@ import React from 'react'
 import { graphql } from 'gatsby'
 import Layout from '../components/layout'
 import { GatsbyImage } from 'gatsby-plugin-image';
+import { Link } from "gatsby"
+import Seo from "../components/seo"
+import * as styles from "../components/index.module.css"
 
 const UnitGallery = ({data}) =>{
-    console.log(data);
+    
     const { title, submissions } = data.contentfulUnit;
-
+    console.log(submissions);
     return(
         <Layout>
             <h1>{title}</h1>
+            <ul >
+                {
+                    submissions.map(edge => (
+                    <li key={edge.slug}>
+                        <Link to={`/${edge.slug}`}>                        <GatsbyImage 
+                            image={edge.finalArtwork.gatsbyImageData} alt=""
+                        /></Link>
+                    </li>
+                    ))
+                }
+            </ul>
         </Layout>
     )
 }
@@ -21,12 +35,14 @@ export const pageQuery = graphql`
     query unitQuery($slug: String!){
         contentfulUnit(slug: {eq: $slug}){
             title
+            slug
             submissions{
                 slug
                 finalArtwork{gatsbyImageData(
                 layout: CONSTRAINED
                 placeholder: BLURRED
-                width: 600
+                width: 300
+                
                 )} 
             }
         }
